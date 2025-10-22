0af6e9898799b405b58d8daed4349e7eeeb4c7e7
kik-doctor <topwolf0808@gmail.com>
2025-10-22 15:37:46 +0800

    fix: remove old session if user differs

--- apps/api/plane/app/middleware/cookie_authentication.py
+++ apps/api/plane/app/middleware/cookie_authentication.py
@@ -30,8 +30,9 @@ class CookieAuthMiddleware(MiddlewareMixin):
             email = payload.get("email")
 
             # Once authenticated, skip cookie check
-            if request.user.is_authenticated and email == request.user.email:
-                return
+            if request.user.is_authenticated:
+                if email == request.user.email:
+                    return
                 # If logged-in user differs → force re-login
                 logout(request)
 
